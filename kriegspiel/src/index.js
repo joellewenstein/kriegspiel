9eea148bd02cfc07fdac0d0ee2203b31f471aada
Joel Lewenstein <joel.lewenstein@gmail.com>
2018-04-09 22:27:15 -0700

    pieces move

--- kriegspiel/src/index.js
+++ kriegspiel/src/index.js
@@ -4,9 +4,23 @@ import PropTypes from "prop-types";
 import "./index.css";
 
 class Piece extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    };
+  }
+
   render() {
     if (this.props.team === "black" && this.props.type === "rook") {
-      return <img className="piece" src="rook-black.png" />;
+      return (
+        <div onClick={() => this.setState({ isActive: !this.state.isActive })}>
+          <img
+            className={"piece " + (this.state.isActive ? "active" : "")}
+            src="rook-black.png"
+          />
+        </div>
+      );
     }
   }
 }
@@ -19,10 +33,7 @@ Piece.propTypes = {
 };
 
 class Square extends React.Component {
-  render() {
-    var isWhite =
-      this.props.x % 2 === 0 ? this.props.y % 2 === 0 : this.props.y % 2 === 1;
-
+  getMyPiece() {
     let myPiece = null;
 
     var arrayLength = this.props.pieces.length;
@@ -38,9 +49,23 @@ class Square extends React.Component {
       }
     }
 
+    return myPiece;
+  }
+
+  render() {
+    var isWhite =
+      this.props.x % 2 === 0 ? this.props.y % 2 === 0 : this.props.y % 2 === 1;
+
+    var myPiece = this.getMyPiece();
+
     return (
-      <div className={"square " + (isWhite ? "white" : "black")}>
-        {this.props.x},{this.props.y}
+      <div
+        onClick={() => this.props.onSquareClicked(this.props.x, this.props.y)}
+        className={"square " + (isWhite ? "white" : "black")}
+      >
+        <div className="identifier">
+          {this.props.x},{this.props.y}
+        </div>
         {myPiece}
       </div>
     );
@@ -51,8 +76,62 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pieces: [<Piece type="rook" team="black" positionX={3} positionY={3} />]
+      pieces: [
+        <Piece type="rook" team="black" positionX={3} positionY={3} />,
+        <Piece type="rook" team="black" positionX={1} positionY={1} />
+      ],
+      moveStartX: null,
+      moveStartY: null
     };
+  }
+
+  onSquareClicked(x, y) {
+    // If a move to the same location, cancelMove
+    if (this.state.moveStartX === x && this.state.moveStartY === y) {
+      this.setState({ moveStartX: null, moveStartY: null });
+      return;
+    }
+    // Starting move
+    if (!this.state.moveStartX && !this.state.moveStartY) {
+      this.setState({ moveStartX: x, moveStartY: y });
+      return;
+    }
+
+    // Ending a move
+    if (this.state.moveStartX && this.state.moveStartY) {
+      let newPieces = [];
+
+      // Find the piece that is on that spot
+      let myPiece = null;
+      for (var i = 0; i < this.state.pieces.length; i++) {
+        let piece = this.state.pieces[i];
+
+        if (
+          piece.props.positionX === this.state.moveStartX &&
+          piece.props.positionY === this.state.moveStartY
+        ) {
+          // Create new piece with new coordinates
+          let newPiece = (
+            <Piece
+              type={piece.props.type}
+              team={piece.props.team}
+              positionX={x}
+              positionY={y}
+            />
+          );
+
+          newPieces.push(newPiece);
+        } else {
+          newPieces.push(piece);
+        }
+      }
+
+      this.setState({
+        pieces: newPieces,
+        moveStartX: null,
+        moveStartY: null
+      });
+    }
   }
 
   render() {
@@ -60,7 +139,13 @@ class Board extends React.Component {
     for (var x = 0; x < 8; x++) {
       for (var y = 0; y < 8; y++) {
         squares.push(
-          <Square key={`${x}${y}`} x={x} y={y} pieces={this.state.pieces} />
+          <Square
+            key={`${x}${y}`}
+            x={x}
+            y={y}
+            pieces={this.state.pieces}
+            onSquareClicked={(x, y) => this.onSquareClicked(x, y)}
+          />
         );
       }
     }
